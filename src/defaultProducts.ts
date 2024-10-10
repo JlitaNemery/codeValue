77eaa38a0ee10fc70055ee9d6964878d3ddcf346
Eitan Lavi <44952942+JlitaNemery@users.noreply.github.com>
2024-10-10 20:36:22 +0300

    Update defaultProducts.ts

--- src/defaultProducts.ts
+++ src/defaultProducts.ts
@@ -3,15 +3,15 @@ import { Product } from "./types/types";
 const defaultProducts:Product[] = [
     {
         id: 3,
-        name: 'z',
-        description: 'z',
+        name: 'Eitan Lavi',
+        description: 'My name is Eitan',
         price: 40,
         creationDate: 1
     },
     {
         id: 4,
-        name: 'a',
-        description: 'd',
+        name: 'Web Developer',
+        description: `I build the web`,
         price: 40,
         creationDate: new Date().getTime()
     }
